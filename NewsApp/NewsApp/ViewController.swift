d13bf96e7e0e110ecdbb6e4e9051ab683425baf7
Vasili13 <90265333+Vasili13@users.noreply.github.com>
2023-02-02 13:10:56 +0300

    First commit
    First project commit

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Василий Вырвич on 2.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
